1ecf898b13af403a993f32feb21eb1a8fb996063
Karthik P <padiyil2004@gmail.com>
2026-06-21 04:49:02 +0530

    add the image,table and check list in editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -3,6 +3,16 @@
 import React from 'react'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import {TaskList} from '@tiptap/extension-task-list'
+import {TaskItem} from '@tiptap/extension-task-item'
+import { Table } from '@tiptap/extension-table'
+import { TableRow } from '@tiptap/extension-table-row'
+import { TableCell } from '@tiptap/extension-table-cell'
+import { TableHeader } from '@tiptap/extension-table-header'
+import { Image } from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
+
+
 
 export const Editor = () => {
   const editor = useEditor({
@@ -12,7 +22,9 @@ export const Editor = () => {
         class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
       },
     },
-    extensions: [StarterKit],
+    extensions: [StarterKit, TaskList, TaskItem.configure({
+        nested: true,
+      }), Table, TableRow, TableCell, TableHeader, Image ,ImageResize],
     content: '<p>Hello World! 🌎️</p>',
     immediatelyRender: false,
   })
