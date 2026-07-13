1e16c2b761120b9b1ae91a55b5e0b1c4d694bd13
Karthik P <padiyil2004@gmail.com>
2026-07-13 10:19:55 +0530

    made templates with carousal of shadcn

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { Navbar } from './Navbar'
-import { TemplatesGallery } from './templateGallery'
+import { TemplatesGallery } from './templatesGallery'
 
 const Home = () => {
   return (
