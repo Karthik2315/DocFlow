6d159432662515f2688737453d71c7251c226798
Karthik P <padiyil2004@gmail.com>
2026-07-13 15:31:03 +0530

    added the functionality for routing when clicking template

--- src/app/(home)/templatesGallery.tsx
+++ src/app/(home)/templatesGallery.tsx
@@ -8,11 +8,24 @@ import {
 } from "@/components/ui/carousel"
 import { templates } from "@/constants/templates";
 import { cn } from "@/lib/utils";
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
 
 
 export const TemplatesGallery = () => {
-  
-  const isCreating = false;
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+  const [isCreating,setIsCreating] = useState(false);
+  const onTemplateClick = (title:string,intialContent:string) => {
+    setIsCreating(true);
+    create({title,intialContent}).then((documentId) => {
+      router.push(`/documents/${documentId}`)
+    }).finally(() => {
+      setIsCreating(false);
+    })
+  }
   return (
     <div className="bg-[#F1F3F4]">
       <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
@@ -22,7 +35,7 @@ export const TemplatesGallery = () => {
             {templates.map((template) => (
               <CarouselItem key={template.id} className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 2xl:basis-[14.285714%] pl-4 ">
                 <div className={cn("aspect-[3/4] flex flex-col gap-y-2.5",isCreating && "pointer-events-none opacity-50")}>
-                  <button disabled={isCreating} onClick={() => {}} style={{
+                  <button disabled={isCreating} onClick={() => onTemplateClick(template.label,"")} style={{
                     backgroundImage: `url(${template.imageUrl})`,
                     backgroundSize:"cover",
                     backgroundPosition:"center",
