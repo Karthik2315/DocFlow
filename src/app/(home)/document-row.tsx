95ff8efe16e15b2c243649eba44f3d16abe422d0
Karthik P <padiyil2004@gmail.com>
2026-07-15 10:59:58 +0530

    now if you cilck : it will not trigger router

--- src/app/(home)/document-row.tsx
+++ src/app/(home)/document-row.tsx
@@ -4,17 +4,22 @@ import {SiGoogledocs} from "react-icons/si";
 import { Building2Icon, CircleUserIcon } from "lucide-react";
 import { format } from "date-fns"
 import { DocumentMenu } from "./document-menu";
+import { useRouter } from "next/navigation";
 
 interface DocumentRowProps {
   document : Doc<"documents"> ;
 };
 
 export const DocumentRow = ({document}:DocumentRowProps) => {
+  const router = useRouter();
   const onNewTabClick = (id:string) => {
     window.open(`/documents/${id}`,"_blank");
   }
+  const onRowClick = (id:string) => {
+    router.push(`/documents/${id}`);
+  }
   return (
-    <TableRow className="cursor-pointer border-none">
+    <TableRow className="cursor-pointer border-none" onClick={() => onRowClick(document._id)}>
       <TableCell className="w-[50px]">
         <SiGoogledocs className="size-6 fill-blue-500"/>
       </TableCell>
@@ -28,7 +33,7 @@ export const DocumentRow = ({document}:DocumentRowProps) => {
       <TableCell className="text-slate-400">
         {format(new Date(document._creationTime),"MMM dd, yyyy")}
       </TableCell>
-      <TableCell className="flex justify-end ml-auto">
+      <TableCell className="flex justify-end ml-auto" onClick={(e) => e.stopPropagation()}>
         <DocumentMenu documentId={document._id} title={document.title} onNewTab={onNewTabClick} />
       </TableCell>
     </TableRow>
