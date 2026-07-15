267fd47f98f986f6733446687d621373a26df4df
Karthik P <padiyil2004@gmail.com>
2026-07-15 12:04:27 +0530

    implemented search feature

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,9 +4,11 @@ import { Navbar } from './Navbar'
 import { TemplatesGallery } from './templatesGallery'
 import { api } from '../../../convex/_generated/api';
 import { DocumentsTable } from './documents-table';
+import { useSearchParam } from '@/hooks/use-search-params';
 
 const Home = () => {
-  const { results, status, loadMore } = usePaginatedQuery(api.documents.get,{},{ initialNumItems:5 });
+  const [search] = useSearchParam("search");
+  const { results, status, loadMore } = usePaginatedQuery(api.documents.get,{search},{ initialNumItems:5 });
   return (
     <div className='min-h-screen flex flex-col'>
       <div className='fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4'>
